87ee14aebb0b9ba37c9be3eec1a1207b4adfd1b1
WistonThiago <wthyago-2000@hotmail.com>
2024-04-02 20:55:54 -0300

    Resolvida a tarefa 7 com tratamento de erros incluso.

--- condicionais.py
+++ condicionais.py
@@ -4,4 +4,20 @@ def verificar_maioridade():
         print("Você é maior de idade")
     else:
         print("Você é menor de idade!")
-verificar_maioridade()
+#verificar_maioridade()
+
+def numeroPar():
+    par = False
+    while (par == False):
+      try:
+        numero_par = int(input("Digite um número par: "))
+        if (numero_par % 2 == 0 and numero_par != 0):
+          print("Você digitou um número par, que foi:", numero_par)
+          break
+        elif (numero_par == 0):
+          print("Você digitou zero...")
+        else:
+          print("Não é par!")
+      except:
+        print("[404] Entrada inválida...")
+numeroPar()
