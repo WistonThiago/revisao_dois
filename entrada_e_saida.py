cdd7bd0a549c868d2bbee28c7b2ad05034f023a3
WistonThiago <wthyago-2000@hotmail.com>
2024-04-02 20:15:37 -0300

    Resolve a tarefa 2.

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -1,4 +1,12 @@
 def saudar_usuario():
     nome = input("Qual é o seu nome? ")
     print("Olá,", nome, "! Seja bem-vindo (a)!")
-saudar_usuario()
+#saudar_usuario()
+
+def soma():
+    sumvalue1 = int(input("Digite um número: "))
+    sumvalue2 = int(input("Digite outro número: "))
+    sumresult = sumvalue1+sumvalue2
+    print("A soma desses números é: ",sumresult)
+soma()
+
