f8b005a2025f169c20ce5ba8d02a5c116d3a96cf
WistonThiago <wthyago-2000@hotmail.com>
2024-04-02 20:18:30 -0300

    Resolve a tarefa 3.

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -8,5 +8,11 @@ def soma():
     sumvalue2 = int(input("Digite outro número: "))
     sumresult = sumvalue1+sumvalue2
     print("A soma desses números é: ",sumresult)
-soma()
+#soma()
 
+def multiply():
+    multvalue1 = int(input("Digite um número: "))
+    multvalue2 = int(input("Digite outro número: "))
+    multresult = multvalue1*multvalue2
+    print("A multiplicação desses números é: ",multresult)
+multiply()
