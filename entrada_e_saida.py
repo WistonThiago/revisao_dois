07e58ee06b80ac0ea9de711b6d20e0306c521f3d
WistonThiago <wthyago-2000@hotmail.com>
2024-04-02 20:27:03 -0300

    Resolve a tarefa 5 e libera as funções restantes.

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -1,21 +1,21 @@
 def saudar_usuario():
     nome = input("Qual é o seu nome? ")
     print("Olá,", nome, "! Seja bem-vindo (a)!")
-#saudar_usuario()
+saudar_usuario()
 
 def soma():
     sumvalue1 = int(input("Digite um número: "))
     sumvalue2 = int(input("Digite outro número: "))
     sumresult = sumvalue1+sumvalue2
     print("A soma desses números é: ",sumresult)
-#soma()
+soma()
 
 def multiply():
     multvalue1 = int(input("Digite um número: "))
     multvalue2 = int(input("Digite outro número: "))
     multresult = multvalue1*multvalue2
     print("A multiplicação desses números é: ",multresult)
-#multiply()
+multiply()
 
 def media():
     mediavalue1 = int(input("Digite um número: "))
@@ -23,3 +23,10 @@ def media():
     mediaresult = (mediavalue1+mediavalue2)/2
     print("A média desses números é: ",mediaresult)
 media()
+
+def imc():
+    altura = float(input("Digite a sua altura: "))
+    peso = float(input("Digite o seu peso: "))
+    imcresult = round((peso/altura), 2)
+    print("O seu IMC é de: ",imcresult)
+imc()
