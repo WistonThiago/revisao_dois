f5b75b5686101b18a2381ed563b5b44d73fa9ff0
WistonThiago <wthyago-2000@hotmail.com>
2024-04-02 21:09:50 -0300

    Resolvida a tarefa 8 com tratamento de erros incluso.

--- condicionais.py
+++ condicionais.py
@@ -20,4 +20,21 @@ def numeroPar():
           print("Não é par!")
       except:
         print("[404] Entrada inválida...")
-numeroPar()
+#numeroPar()
+
+def resultadoFinal():
+   nota = False
+   while (nota == False):
+      try:
+        nota_aluno = float(input("Digite a sua nota: "))
+        if(nota_aluno >= 7 and nota_aluno <= 10 and nota_aluno > 0):
+            print("Parabéns, você passou! A sua nota foi: ",nota_aluno)
+            break
+        elif (nota_aluno > 10 or nota_aluno < 0):
+            print("Digite uma nota válida")
+        else:
+            print("Infelizmente você não foi aprovado, você tirou: ",nota_aluno)
+            break
+      except:
+            print("Entrada inválida")
+resultadoFinal()
